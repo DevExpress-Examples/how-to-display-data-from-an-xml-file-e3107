adbd5062df1140040fe51c194535e04155f589cf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DataFromXML/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Xml.Schema;
+
+namespace DataFromXML {
+    public partial class MainPage : UserControl {
+
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = GetData();
+        }
+
+        ObservableCollection<Contact> GetData() {
+
+            XDocument doc = XDocument.Load("/DataFromXML;component/Contacts.xml");
+
+            var items = from item in doc.Descendants("Contacts")
+                        select new Contact() {
+                            FirstName = item.Element("FirstName").Value,
+                            LastName = item.Element("LastName").Value,
+                            Company = item.Element("Company").Value,
+                            ID = int.Parse(item.Element("ID").Value)
+                        };
+
+            ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+            foreach (Contact contact in items) {
+                contacts.Add(contact);
+            }
+            return contacts;
+        }
+    }
+
+    public class Contact {
+        public int ID {
+            get;
+            set;
+        }
+        public string FirstName {
+            get;
+            set;
+        }
+        public string LastName {
+            get;
+            set;
+        }
+        public string Company {
+            get;
+            set;
+        }
+    }
+}
---
+++ VB/DataFromXML/MainPage.xaml.vb
@@ -0,0 +1,83 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports System.Xml
+Imports System.Xml.Linq
+Imports System.IO
+Imports System.Collections.ObjectModel
+Imports System.ComponentModel
+Imports System.Xml.Schema
+
+Namespace DataFromXML
+	Partial Public Class MainPage
+		Inherits UserControl
+
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = GetData()
+		End Sub
+
+		Private Function GetData() As ObservableCollection(Of Contact)
+
+			Dim doc As XDocument = XDocument.Load("/DataFromXML;component/Contacts.xml")
+
+			Dim items = _
+				From item In doc.Descendants("Contacts") _
+				Select New Contact() With {.FirstName = item.Element("FirstName").Value, .LastName = item.Element("LastName").Value, .Company = item.Element("Company").Value, .ID = Integer.Parse(item.Element("ID").Value)}
+
+			Dim contacts As New ObservableCollection(Of Contact)()
+			For Each contact As Contact In items
+				contacts.Add(contact)
+			Next contact
+			Return contacts
+		End Function
+	End Class
+
+	Public Class Contact
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateFirstName As String
+		Public Property FirstName() As String
+			Get
+				Return privateFirstName
+			End Get
+			Set(ByVal value As String)
+				privateFirstName = value
+			End Set
+		End Property
+		Private privateLastName As String
+		Public Property LastName() As String
+			Get
+				Return privateLastName
+			End Get
+			Set(ByVal value As String)
+				privateLastName = value
+			End Set
+		End Property
+		Private privateCompany As String
+		Public Property Company() As String
+			Get
+				Return privateCompany
+			End Get
+			Set(ByVal value As String)
+				privateCompany = value
+			End Set
+		End Property
+	End Class
+End Namespace
